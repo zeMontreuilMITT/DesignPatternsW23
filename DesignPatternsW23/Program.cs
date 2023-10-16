9c1048084520dcf60e75c9a0233e594d3de26111
Zacharie Montreuil <zacharie.montreuil@mitt.ca>
2023-10-16 11:19:36 -0500

    Add initial example

--- DesignPatternsW23/Program.cs
+++ DesignPatternsW23/Program.cs
@@ -1,2 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Duck newDuck;
+
+string type = Console.ReadLine();
+
+// at compile-time, the system does not know which implementation of Duck will be used
+if(type == "a")
+{
+    newDuck = new Mallard();
+}
+else
+{
+    newDuck = new NoiseMakingDuckThatCantFly();
+}
+
+// since all of these methods are defined on the parent class they will be available in the child class
+newDuck.Float();
+newDuck.Display();
+newDuck.PerformQuack();
+newDuck.PerformFly();
+
+public abstract class Duck
+{
+    public abstract void Display(); // Display method will always change in implementation amongst all child classes
+    public void Float() // Swim method will NEVER change amongst child classes, so we define it here in a way that cannot be altered
+    {
+        Console.WriteLine("The duck floats on water");
+    }
+
+    //=== BEHAVIOURS === 
+    // behaviours that may change and may be reused in different combinations amongst different subclasses
+    // at compile-time, the parent class does not know which implementation it will use
+    // however, it DOES know that it will have the methods defined on the interface available
+    protected IFlyBehaviour _flyBehaviour;
+    protected IQuackBehaviour _quackBehaviour;
+
+    public void PerformFly()
+    {
+        // at compile-time, the parent class does not know what this method will do, but it does know that it can call it
+        _flyBehaviour.Fly();
+    }
+
+    public void PerformQuack()
+    {
+        _quackBehaviour.Quack();
+    }
+}
+
+public class Mallard: Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("A grey duck with a striking green colour on the head");
+    }
+
+    public Mallard()
+    {
+        _flyBehaviour = new FlyWithWings();
+        _quackBehaviour = new QuackLikeADuck();
+    }
+}
+
+public class NoiseMakingDuckThatCantFly: Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("This duck for some reason does not have wings and only quacks.");
+    }
+
+    public NoiseMakingDuckThatCantFly()
+    {
+        _flyBehaviour = new FlyUnable();
+        _quackBehaviour = new QuackLikeADuck();
+    }
+}
+
+public interface IFlyBehaviour
+{
+    void Fly();
+}
+
+public class FlyWithWings : IFlyBehaviour
+{
+    public void Fly()
+    {
+        Console.WriteLine("The duck flaps its wings and flies in a circle");
+    }
+}
+
+public class FlyUnable: IFlyBehaviour
+{
+    public void Fly()
+    {
+        Console.WriteLine("This duck cannot fly :( ");
+    }
+}
+
+// =====
+
+public interface IQuackBehaviour
+{
+    void Quack();
+}
+
+public class QuackLikeADuck: IQuackBehaviour
+{
+    public void Quack()
+    {
+        Console.WriteLine("The duck quacks the way most ducks do");
+    }
+}
